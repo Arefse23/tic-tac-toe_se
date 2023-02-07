86f4cf7a6a2444772d33ee511415d5c9a3101457
Aref Rabea <aref.h.rabea@gmail.com>
2023-02-07 23:46:15 +0300

    fix

--- vx.js
+++ vx.js
@@ -1,12 +1,22 @@
 
 // ===================================Necessary Var=========================================
 const cells = document.querySelectorAll('.cell')
-const statusText = document.getElementById('statusText')
+const stat_text = document.getElementById('statusText')
+const stat_textf = document.getElementById('statusTextf')
 const restartbtn = document.getElementById('restartBtn')
-const ps1 = document.getElementById('ps1')
-const ps2 = document.getElementById('ps2')
+const ps1 = document.getElementById('Ps1')
+const ps2 = document.getElementById('Ps2')
+const aud = document.getElementById('aud')
+const aud2 = document.getElementById('aud2')
 let ss = 0;
-    
+let ss2 = 0;
+ 
+
+aud.src ="/Project/XO/Opening-Credits-_-Game-of-Thrones-_-Season-8-_HBO_.mp3"
+aud2.src="/Project/XO/sword-sound-effects-for-Editing.mp3"
+aud.volume =0.5;
+aud2.volume = 0.8;
+
 const winConditions =[
     [0,1,2],
     [3,4,5],
@@ -36,23 +46,17 @@ gamestart();
 
 function gamestart(){
 
-cells.forEach(cell => cell.addEventListener('click', cellClicked))
+cells.forEach(cell => cell.addEventListener('click', c_clicked))
 restartbtn.addEventListener('click',resartGame);
-statusText.textContent = `${currentPlayer}'s turn`
+stat_text.textContent = `${currentPlayer}'s turn`
 running= 1;
 }
 
 
 
 
-
-
-
-
-
-
 // ===================================Game Commands=========================================
-function cellClicked(){
+function c_clicked(){
 const cellIndex = this.getAttribute('id');
 
 if(options[cellIndex] != "" || !running){
@@ -62,6 +66,7 @@ if(options[cellIndex] != "" || !running){
 updateCell(this ,cellIndex);
 changePlayer();
 checkWinner();
+aud.play();
 }
 
 
@@ -79,7 +84,7 @@ function updateCell(cell, index){
 
 function changePlayer(){
     currentPlayer=(currentPlayer === "X") ? "O" :"X";
-    statusText.textContent =`${currentPlayer}'s turn `
+    stat_text.textContent =`${currentPlayer}'s turn `
 }
 
 
@@ -88,20 +93,21 @@ function changePlayer(){
 
 function checkWinner(){
 
-    let roundWon = false;
+    let roundWon = 0;
 
     for(let i = 0; i < winConditions.length; i++){
         const condition = winConditions[i];
-        const cellA = options[condition[0]];
-        const cellB = options[condition[1]];
-        const cellC = options[condition[2]];
+        const cho1 = options[condition[0]];
+        const cho2 = options[condition[1]];
+        const cho3 = options[condition[2]];
 
-        if(cellA == "" || cellB == "" || cellC == ""){
-            continue;
-        }
-        if(cellA == cellB && cellB == cellC){
-            roundWon = true;
-            console.log(cellA,cellB,cellC)
+        if(cho1 == "" || 
+        cho2 == "" ||
+        cho3 == ""){continue;}
+
+
+        if(cho1 == cho2 && cho2 == cho3){
+            roundWon = 1;
             break;
         }
     }
@@ -110,9 +116,10 @@ function checkWinner(){
                         // ============================================
 
 
-    if(roundWon){
+    if(roundWon ===1){
         currentPlayer = (currentPlayer === "X")? "O" :"X";
-        statusText.textContent = `${currentPlayer} wins!`;
+        stat_text.textContent = `${currentPlayer} wins!`;
+        aud2.play();
         running = 0;
 
         
@@ -120,15 +127,42 @@ function checkWinner(){
         if(currentPlayer === "O"){
             ++ss
             ss.toString();
-            console.log(ss);
-            ps1.textContent =`${ss}`
-            
+            ps2.textContent =`${ss}`
+            if(ss >1){
+                stat_text.textContent = `${currentPlayer}' Double wins or nothing!`;
+            }
+            if(ss>2){
+                stat_text.textContent = `${currentPlayer}' Hat trick Wins!!`;
+            }
+            if(ss>3){
+                stat_text.textContent = `${currentPlayer}' Legend Charcter!!!`;
+                stat_textf.textContent ="Final Winner is Player Two"
+                
+            }
             
         }
-        console.log(options);
+        if(currentPlayer === "X"){
+            ++ss2
+            ss2.toString();
+            ps1.textContent =`${ss2}`
+            
+            if(ss2 >1){
+                stat_text.textContent = `${currentPlayer}' Double wins or nothing!`;
+            }
+            if(ss2>2){
+                stat_text.textContent = `${currentPlayer}' Hat trick Wins!!`;
+            }
+            if(ss2>3){
+                stat_text.textContent = `${currentPlayer}' Legend Charcter!!!`;
+                stat_textf.textContent ="Final Winner is Player one"
+               
+            }
+            
+
+        }
     }
     else if(!options.includes("")){
-        statusText.textContent = `Draw!`;
+        stat_text.textContent = `Draw!`;
         running = 0;
     }
    
@@ -141,8 +175,13 @@ function checkWinner(){
 function resartGame(){
     currentPlayer = "X";
     options = ["", "", "", "", "", "", "", "", ""];
-    statusText.textContent = `${currentPlayer}'s turn`;
+    stat_text.textContent = `${currentPlayer}'s turn`;
     cells.forEach(cell => cell.textContent = "");
+    let cur2 =["X","O","X","O"]
+    let rannd2 = Math.floor(Math.random()*cur.length)
+    currentPlayer = cur2[rannd2];
+    stat_text.textContent =`${currentPlayer}'s turn `
+    
     running = 1;
 }
 
